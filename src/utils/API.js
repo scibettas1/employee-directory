82fae1a4d868f683d3af3b8472813dd4f94ff18e
Sam Scibetta <sammieeccentric@gmail.com>
2021-01-19 17:58:31 -0500

    realized the ajax activity was a better starting point and deleted the previous files

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
-const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+const BASEURL = "https://randomuser.me/api/";
+const APIKEY = "&api_key=6EZH-4KCX-2SQ1-C5OB";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
   search: function(query) {
+      console.log(query)
     return axios.get(BASEURL + query + APIKEY);
   }
 };
