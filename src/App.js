4cecbe523563ea7331070c6aeaa243fabad5a0dc
Sam Scibetta <sammieeccentric@gmail.com>
2021-01-23 07:05:45 -0500

    pupster app was a better starting point. just started over

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
-import React, { Component } from "react";
-import EmployeeCard from "./components/EmployeeCard";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Discover from "./pages/Discover";
+import About from "./pages/About";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
 
-class App extends Component {
-  // Setting this.state.friends to the friends json array
-  state = {
-    friends
-  };
-
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
-
-  // Map over this.state.friends and render a FriendCard component for each friend object
-  render() {
-    return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <EmployeeCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/discover" component={Discover} />
+          <Route exact path="/search" component={Search} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
-const BASEURL = "https://randomuser.me/api/";
-const APIKEY = "&api_key=6EZH-4KCX-2SQ1-C5OB";
+// Export an object containing methods we'll use for accessing the Dog.Ceo API
 
-// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-  search: function(query) {
-      console.log(query)
-    return axios.get(BASEURL + query + APIKEY);
+  getRandomDog: function() {
+    return axios.get("https://dog.ceo/api/breeds/image/random");
+  },
+  getDogsOfBreed: function(breed) {
+    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
+  },
+  getBaseBreedsList: function() {
+    return axios.get("https://dog.ceo/api/breeds/list");
   }
 };
