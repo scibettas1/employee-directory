73edb4e90515f5b71be6fc6b5d490cf4ed4a821f
Sam Scibetta <sammieeccentric@gmail.com>
2021-02-18 18:31:52 -0500

    finally rendering more than one person and filtering by name.

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -4,7 +4,6 @@ import Row from "./Row";
 import Col from "./Col";
 import Card from "./Card";
 import SearchForm from "./SearchForm";
-import EmployeeDetail from "./EmployeeDetail";
 import API from "../utils/API";
 
 class EmployeeContainer extends Component {
@@ -13,38 +12,41 @@ class EmployeeContainer extends Component {
     search: ""
   };
 
+
   componentDidMount() {
     API.search()
       .then((res) => {
         //console.log(res.data.results)
-        var employeeArray = res.data.results.map((employee)=>{
+        var employeeArray = res.data.results.map((employee) => {
           //console.log(employee)
           return {
             name: employee.name.first + " " + employee.name.last,
             country: employee.location.country,
             email: employee.email,
-            picture: employee.picture.thumbnail
+            picture: employee.picture.large
           }
         })
-        //console.log(employeeArray)
-         this.setState({ result: employeeArray })
-         console.log(this.state)
-        })
+        console.log(employeeArray)
+        this.setState({ result: employeeArray })
+        //console.log(this.state)
+      })
       .catch(err => console.log(err));
   };
 
   handleInputChange = event => {
-    const value = event.target.value;
-    const name = event.target.name;
+    const value = event.target.value.toLowerCase();
+    const temp = this.state.result.filter(employee => employee.name.toLowerCase().includes(value));
     this.setState({
-      [name]: value
+      search: value,
+      result: temp,
     });
   };
 
   // When the form is submitted, search the OMDB API for the value of `this.state.search`
   handleFormSubmit = event => {
     event.preventDefault();
-    this.componentDidMount(this.state.search);
+    // this.componentDidMount(this.state.search);
+    console.log(this.state.search)
   };
 
   render() {
@@ -53,15 +55,21 @@ class EmployeeContainer extends Component {
         <Row>
           <Col size="md-8">
             <Card
-              heading={this.state.result.Name || "Search for an Employee"}
-            >
+              heading={this.state.result.Name || "Search for an Employee"}>
               {this.state.result.length !== 0 ? (
-                <EmployeeDetail
-                  employee={this.state.result[0]}
-                />
+                this.state.result.map(employee => {
+                  return (
+                    <div>
+                      <img src={employee.picture} alt={employee.title} className="img-fluid" />
+                      <p>Name: {employee.name}</p>
+                      <p>Country: {employee.country}</p>
+                      <p>Email: {employee.email}</p>
+                    </div>
+                  );
+                })
               ) : (
-                <h3>No Results to Display</h3>
-              )}
+                  <h3>No Results to Display</h3>
+                )}
             </Card>
           </Col>
           <Col size="md-4">
--- src/components/EmployeeDetail.js
+++ src/components/EmployeeDetail.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function EmployeeDetail(props) {
-  console.log(props)
+  //console.log(props)
   return (
     <div>
-      <img alt={props.title} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} />
-      <p>Name: {props.employee.name}</p>
+      <img src={props.picture} alt={props.title} className="img-fluid" />
+      <p>Name: {props.name}</p>
       <p>Country: {props.employee.country}</p>
       <p>Email: {props.employee.email}</p>
     </div>
